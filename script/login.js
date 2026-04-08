e162677ccbe9fe428a1f4b1e828111c8b37d1bbd
Tanvir Lokman Tamim <tanvirlokmantamim@gmail.com>
2026-04-09 00:27:35 +0600

    updated

--- script/login.js
+++ script/login.js
@@ -3,7 +3,7 @@ const signInBtn = document.getElementById("sign-btn");
 if (signInBtn) {
     signInBtn.addEventListener("click", function () {
         const username = document.getElementById("inputUser").value;
-        const password = document.getElementById("iinputPass").value;
+        const password = document.getElementById("inputPass").value;
 
         if (username === "admin" && password === "admin123") {
             window.location.assign("home.html")
